94c08fa86dd4afeaa484ca82e61d809363b25c20
Srikanth Marrapu <srikanth23111995@gmail.com>
2021-09-21 08:13:14 +0530

    Updates

---
+++ precache-manifest.56ece2d58880ed3b31a2a9506cbea215.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a41c3e96dd43ab0f7fe3165682cf2c60",
+    "url": "/Hola/index.html"
+  },
+  {
+    "revision": "6162fc0e4d76d4ffcddc",
+    "url": "/Hola/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "012cfd69cd8178284439",
+    "url": "/Hola/static/js/2.cc4f5b9d.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/Hola/static/js/2.cc4f5b9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6162fc0e4d76d4ffcddc",
+    "url": "/Hola/static/js/main.d48ee5a7.chunk.js"
+  },
+  {
+    "revision": "09566430e12c15b05a35",
+    "url": "/Hola/static/js/runtime-main.d7e9c336.js"
+  }
+]);
